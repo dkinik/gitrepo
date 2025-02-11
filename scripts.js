24f7506c772d3aeb990928f09a929306d72350c8
dkinik <kini.deepa11@gmail.com>
2025-02-11 09:29:46 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('myButton');
-    button.addEventListener('click', function() {
-        alert('Button was clicked!');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    // Ensure the element exists before trying to add an event listener
+    const loginButton = document.getElementById('loginButton');
+    
+    if (loginButton) {
+        loginButton.addEventListener('click', function () {
+            // Your code for handling the button click here
+            window.location.href = 'https://github.com/login/oauth/authorize?client_id=Ov23li5blY0ABDArPh1W&redirect_uri=https://dkinik.github.io/gitrepo/callback&scope=read:user';
+        });
+    } else {
+        console.error('Login button not found.');
+    }
 });
